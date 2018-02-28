504bf2742e346ce2b9812047bbbd3688c24e60d8
TangWeilun <tang_wlhs@163.com>
2018-02-28 13:04:55 +0800

    create test

---
+++ clientplan/src/main/java/com/zhuri/clientplan/ClientplanApplication.java
@@ -0,0 +1,12 @@
+package com.zhuri.clientplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientplanApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zhuri/ZhuriApplication.java
@@ -0,0 +1,12 @@
+package com.zhuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhuriApplication.class, args);
+	}
+}
