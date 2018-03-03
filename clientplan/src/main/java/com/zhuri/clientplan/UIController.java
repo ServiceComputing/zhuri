fc7c3504d91f0ba4908855e012002ec82a1f37ea
cplayer <zhcplayer@gmail.com>
2018-03-03 15:04:28 +0800

    add basic test cases for UI

--- clientplan/src/main/java/com/zhuri/clientplan/UIController.java
+++ clientplan/src/main/java/com/zhuri/clientplan/UIController.java
@@ -1,7 +1,15 @@
 package com.zhuri.clientplan;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.xml.ws.RequestWrapper;
 
 @Controller
 public class UIController {
+    @RequestMapping(value = "/planModify", method = RequestMethod.GET)
+    public String planModify() {
+        return "planModify";
+    }
 }
